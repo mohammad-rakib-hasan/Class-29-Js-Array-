2edaf3c4ff5c3927a258093c04e7ac61ffe07f59
mohammad-rakib-hasan <bdprakib9@gmail.com>
2024-10-19 23:07:28 +0600

    My js Array

---
+++ main.js
@@ -0,0 +1,60 @@
+
+                // 1D Array...
+
+let indexArray =[99,89,76,25,36,42,11]
+
+console.log(indexArray[3]);
+console.log(indexArray[6]);
+console.log(indexArray[5]);
+
+
+// => Associative Array....
+
+
+let student = {name:"Rakib", age: 21, Roll: 662548 , Reg: 1502209456 , Phone: 01991854563};
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.Roll);
+console.log(student.Reg);
+console.log(student.Phone);
+
+// 2D Array...
+
+let twoDimensionalArray =[
+    [100, 11, 26, 93, 40, 31],
+    [76, 45, 24, 98, 70, 19],
+    [29, 38, 59, 68, 88, 10],
+    [55, 65, 85, 95, 15, 25]
+];
+ 
+console.log(twoDimensionalArray[0][0]);
+console.log(twoDimensionalArray[0][1]);
+console.log(twoDimensionalArray[0][2]);
+console.log(twoDimensionalArray[0][3]);
+console.log(twoDimensionalArray[0][4]);
+console.log(twoDimensionalArray[0][5]);
+
+console.log(twoDimensionalArray[1][0]);
+console.log(twoDimensionalArray[1][1]);
+console.log(twoDimensionalArray[1][2]);
+console.log(twoDimensionalArray[1][3]);
+console.log(twoDimensionalArray[1][4]);
+console.log(twoDimensionalArray[1][5]);
+
+console.log(twoDimensionalArray[2][0]);
+console.log(twoDimensionalArray[2][1]);
+console.log(twoDimensionalArray[2][2]);
+console.log(twoDimensionalArray[2][3]);
+console.log(twoDimensionalArray[2][4]);
+console.log(twoDimensionalArray[2][5]);
+
+console.log(twoDimensionalArray[3][0]);
+console.log(twoDimensionalArray[3][1]);
+console.log(twoDimensionalArray[3][2]);
+console.log(twoDimensionalArray[3][3]);
+console.log(twoDimensionalArray[3][4]);
+console.log(twoDimensionalArray[3][5]);
+
+
+
